fd167364814210925fa8e44e3ff84eaa95275855
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-05-20 17:44:02 +0200

    Adding new functionality and connecting to the new API

---
+++ AG_webD2/AG_webD2/RESTfulClient.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace AG_webD2
+{
+    public class RESTfulClient
+    {
+        private string baseURL = null;
+        private string message = null;
+        private static RESTfulClient instance = null;
+
+        private RESTfulClient(string baseURL)
+        {
+            this.baseURL = baseURL;
+            this.message = string.Empty;
+        }
+
+        public static void InitializeClent(string baseURL)
+        {
+            if (string.IsNullOrEmpty(baseURL))
+                return;
+
+            baseURL = baseURL.Last() == '/' ? baseURL : baseURL + '/';
+
+            instance = new RESTfulClient(baseURL);
+        }
+
+        public static RESTfulClient Instance { get { return instance; } }
+
+        public string Message { get => message; }
+
+        public T GET<T>(string directive)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{baseURL}{directive}");
+            string JsonObj;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    //Read response
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    StringBuilder resp = new StringBuilder();
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        resp.Append(line).Append("\n");
+                    }
+
+                    reader.Close();
+                    response.Close();
+
+                    JsonObj = resp.ToString();
+
+                    message = "API call was successful";
+
+                    // Use Json object
+                    return JsonConvert.DeserializeObject<T>(JsonObj);
+
+                }
+                else
+                {
+                    message = ("API call failed. Response code: " + response.StatusCode);
+                }
+            }
+            catch (WebException ex)
+            {
+                message = ($"Connection failed: {ex.Message}");
+                return default;
+            }
+
+            return default;
+        }
+    }
+}
---
+++ AG_webD2/AG_webD2/viewDocument.aspx.cs
@@ -0,0 +1,49 @@
+﻿using AG_webD2.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AG_webD2
+{
+    public partial class viewDocument : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RESTfulClient client = RESTfulClient.Instance;
+
+            // Check type of user
+            User user = (User)Session["User"];
+
+            if (user == null || string.IsNullOrEmpty(Request.QueryString["docId"]))
+                Response.Redirect("login.aspx");
+
+            int documentID = int.Parse(Request.QueryString["docId"]);
+            Application application = null;
+            Document document = null;
+
+            switch (user.Role)
+            {
+                case "student":
+                case "assistant":
+                    if (documentID == 0)
+                    {
+                        application = client.GET<Application>($"Applications/student={user.Id}");
+                        documentDiv.InnerHtml = $"\n<iframe src='{application.Transcript}'></iframe>";
+                    }
+                    else
+                    {
+                        document = client.GET<Document>($"Documents/{documentID}");
+                    }
+                    break;
+
+                case "lecturer":
+
+                    break;
+            }
+            // Check the document Id
+        }
+    }
+}
