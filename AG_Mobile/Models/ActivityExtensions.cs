a092a23ad21ab73afd4ef0b8096ead9e6cdd8ce6
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-06-24 20:27:17 +0200

    Description for the Redirect function

--- AG_Mobile/Models/ActivityExtensions.cs
+++ AG_Mobile/Models/ActivityExtensions.cs
@@ -6,6 +6,13 @@ namespace AG_Mobile.Models
 {
     public static class ActivityExtensions
     {
+        /// <summary>
+        /// An extension to redirect to a new page/activity
+        /// </summary>
+        /// <typeparam name="T">Any type that inherits from Activity</typeparam>
+        /// <param name="activity">The activity/Page</param>
+        /// <param name="type">IDK WFT is this... Tehee ;P </param>
+        /// <seealso cref="Activity"/>
         public static void Redirect<T>(this T activity, Type type) where T : Activity
         {
             Context context = activity.BaseContext;
