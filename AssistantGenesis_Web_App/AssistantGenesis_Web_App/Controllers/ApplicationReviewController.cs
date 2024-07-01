cb6c819530cae60810e466684deee82dd7ef6b23
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-07-01 19:39:35 +0200

    Working on the menu bar

--- AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
+++ AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
@@ -11,12 +11,9 @@ namespace AssistantGenesis_Web_App.Controllers
             IEnumerable<Application> applications = RESTfulClient.Instance.GET<IEnumerable<Application>>("Applications/");
 
             if(applications == default)
-            {
-                string message = $"applications is null, please make sure you have the correct implementation and have applications in your system.";
-                return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, Message = message });
-            }
+                return View(new List<Application>());
 
-            return View("ApplicationReview", applications);
+            return View(applications);
         }
     }
 }
