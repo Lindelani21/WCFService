4167697459f59fbedaf6e29d71854b88f831e98e
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-07-02 10:34:58 +0200

    Application Review Details... Unfinished

--- AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
+++ AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
@@ -6,12 +6,16 @@ namespace AssistantGenesis_Web_App.Controllers
 {
     public class ApplicationReviewController : Controller
     {
+        public static Application? SelectedApplication { get; set; }
+
         public IActionResult Index()
         {
             IEnumerable<Application> applications = RESTfulClient.Instance.GET<IEnumerable<Application>>("Applications/");
 
             if(applications == default)
                 return View(new List<Application>());
+
+            SelectedApplication = applications.ElementAt(0);
 
             return View(applications);
         }
