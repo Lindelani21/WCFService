f25e8e5e4420f8919bbf4d48da333cbb745f87fd
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-07-02 12:32:38 +0200

    Created the Login thingie

---
+++ AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssistantGenesis_Web_App.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
