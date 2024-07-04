96a3e63a634970335dd8acce7c84d89ebd76f8bc
NatashaMab <mabairenatasha@gmail.com>
2024-07-04 14:42:55 +0200

    login functionality edits

--- AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
+++ AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/ApplicationReviewController.cs
@@ -6,13 +6,21 @@ namespace AssistantGenesis_Web_App.Controllers
 {
     public class ApplicationReviewController : Controller
     {
+        //creating an instance of an application
         public static Application? SelectedApplication { get; set; }
 
+        //method returning an IActionREsult
         public IActionResult Index()
         {
+            //IEnumerable - collection of items that can be itterated through 
+            //.GET<IEnumerable<Application>> - getting a collection of applications from the API
+            //"Application/" is an API endpoint
             IEnumerable<Application> applications = RESTfulClient.Instance.GET<IEnumerable<Application>>("Applications/");
 
+            //what is the purpose of checking if the applications is defaut?
             if(applications == default)
+
+                //create new list of applications
                 return View(new List<Application>());
 
             SelectedApplication = applications.ElementAt(0);
--- AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/LoginController.cs
+++ AssistantGenesis_Web_App/AssistantGenesis_Web_App/Controllers/LoginController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AssistantGenesis_Web_App.Models;
 
 namespace AssistantGenesis_Web_App.Controllers
 {
     public class LoginController : Controller
     {
+        db dbop = new db();
+
         public IActionResult Index()
         {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MyIndex([Bind] login ad)
+        {
+            int res = dbop.LoginCheck(ad);
+            if (res == 1)
+            {
+                TempData["msg"] = "Welcome";
+            }
+            else
+            {
+                TempData["msg"] = " Invalid login credentials";
+            }
             return View();
         }
     }
