6267a726362ff29798f08c4e5d42b6e0098b8cb4
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-07-12 12:00:37 +0200

    Neating up

--- AssistantGenesis_Web_App/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿Create PROC Sp_login
-@UserId INT,
-@UserPassword VARCHAR(50),
-@IsValid BIT OUT
-As 
-BEGIN
-SET @IsValid = (SELECT COUNT(1) FROM [User] WHERE Id = @UserId AND Password = @UserPassword)
-end
